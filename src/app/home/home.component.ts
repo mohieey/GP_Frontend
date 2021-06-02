cb9e2c7e4e25c7f82183543393afbbc836892b1c
shahiern@yahoo.com <shahiern@yahoo.com>
2021-06-02 15:27:34 +0200

    image and video uploading

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,30 @@ import { TweetService } from '../shared/services/tweet.service';
 })
 export class HomeComponent implements OnInit {
   homePageTweets: TweetDTO[];
+  modal:HTMLElement;
+  modalWrapper:HTMLElement;
+  modalInput:HTMLInputElement;
+
   constructor(private tweetService: TweetService) { }
 
   ngOnInit(): void {
     this.tweetService.getHomePageTweets().subscribe(res => { this.homePageTweets = res; console.log(res) })
+    this.modal = document.querySelector('.modal');
+    this.modalWrapper = document.querySelector('.modal-wrapper');
+    this.modalInput = document.querySelector('.modal-input');
   }
 
+  openPostTweetWindow(){
+    this.modal.style.display = 'block';
+	  this.modalWrapper.classList.add('modal-wrapper-display');
+  }
+
+  closePostTweetWindow(){
+    this.modal.style.display = 'none';
+    this.modalWrapper.classList.remove('modal-wrapper-display');
+
+    if (this.modalInput.value !== '') {
+      this.modalInput.value = '';
+    }
+  }
 }
